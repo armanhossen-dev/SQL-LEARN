a5986e28ab2e5cab340df0d1eadb09a2946bd930
Arman Hossen <2armanhossen@gmail.com>
2026-04-20 02:42:35 +0600

    sql

---
+++ SQL_W3SCHOOLS/sql_syntax.sql
@@ -0,0 +1,19 @@
+-- easy to understand, most of the actions performed on a database are done with sql statements.
+-- sql keywords are NOT case sensitive: select is the same as SELECT
+-- but for understanding sql keyword, i may try to write them in uppercase
+
+-- semicolon after sql statements: some database systems require a semicolon at the end of each SQL statement.
+-- semicolon is the standard way to separate each sql statement in database systems, that allow more than one sql statemnt to be executed in the same call of the server.
+
+-- some of the most important sql commands
+-- SELECT - extracts data from a database
+-- UPDATE - updates data in a database
+-- DELETE - deletes data from a database
+-- INSERT INTO - inserts new data into a database
+-- CREATE DATABASE - creates a new database
+-- ALTER DATABASE - modifies a database
+-- CREATE TABLE - creates a new table
+-- ALTER TABLE - modifies a table
+-- DROP TABLE - deletes a table
+-- CREATE INDEX - creates an index (search key)
+-- DROP INDEX - deletes an index
